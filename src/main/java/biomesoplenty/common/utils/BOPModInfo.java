30a030742766e3a04b7aa8ad45774711b7ef2e97
Adubbz <a.dubbz@hotmail.com>
2014-06-09 14:29:29 +1000

    Capes are now solely kept in the master branch

--- src/main/java/biomesoplenty/common/utils/BOPModInfo.java
+++
@@ -1,7 +0,0 @@
-package biomesoplenty.common.utils;
-
-public class BOPModInfo 
-{
-	public static final String modID = "BiomesOPlenty";
-	public static final String modName = "Biomes O' Plenty";
-}
